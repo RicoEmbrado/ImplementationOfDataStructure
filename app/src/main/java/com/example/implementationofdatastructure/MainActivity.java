ca7d147daf666727d306d6214a3141e5d7da167d
RicoEmbrado <embradorico@gmail.com>
2024-03-14 12:39:02 -0700

    Addition of new names to increase size of the list in order to make it scrollable.

--- app/src/main/java/com/example/implementationofdatastructure/MainActivity.java
+++ app/src/main/java/com/example/implementationofdatastructure/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.implementationofdatastructure;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.widget.LinearLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
 import android.os.Bundle;
 
@@ -10,7 +9,6 @@ public class MainActivity extends AppCompatActivity
 {
     private StackDataStructure stackDataStructure;
     private LinearLayout linearLayout;
-    private ScrollView scrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -20,8 +18,6 @@ public class MainActivity extends AppCompatActivity
 
         stackDataStructure = new StackDataStructure();
         linearLayout = findViewById(R.id.linear_layout);
-        scrollView = findViewById(R.id.scroll_view);
-        scrollView.setTextSize(20);
 
         stackDataStructure.push("Jimmy");
         stackDataStructure.push("Fiona");
@@ -44,6 +40,51 @@ public class MainActivity extends AppCompatActivity
         stackDataStructure.push("Kermit");
         stackDataStructure.push("Amanda");
         stackDataStructure.push("Sierra");
+        stackDataStructure.push("Cody");
+        stackDataStructure.push("Arlan");
+        stackDataStructure.push("Asta");
+        stackDataStructure.push("Acheron");
+        stackDataStructure.push("Gallagher");
+        stackDataStructure.push("Caelus");
+        stackDataStructure.push("Stelle");
+        stackDataStructure.push("Robin");
+        stackDataStructure.push("Sunday");
+        stackDataStructure.push("Sandra");
+        stackDataStructure.push("Kawnepa");
+        stackDataStructure.push("Oliver");
+        stackDataStructure.push("Natasha");
+        stackDataStructure.push("Seele");
+        stackDataStructure.push("Billy");
+        stackDataStructure.push("Lorem");
+        stackDataStructure.push("Ipsum");
+        stackDataStructure.push("Dolor");
+        stackDataStructure.push("Sit");
+        stackDataStructure.push("Amet");
+        stackDataStructure.push("Julia");
+        stackDataStructure.push("Julian");
+        stackDataStructure.push("Tazzyronth");
+        stackDataStructure.push("Adrien");
+        stackDataStructure.push("Polly");
+        stackDataStructure.push("Milo");
+        stackDataStructure.push("Myla");
+        stackDataStructure.push("Jasmine");
+        stackDataStructure.push("Jonathan");
+        stackDataStructure.push("Shawn");
+        stackDataStructure.push("Rico");
+        stackDataStructure.push("Dale");
+        stackDataStructure.push("Danah");
+        stackDataStructure.push("Evan");
+        stackDataStructure.push("Janine");
+        stackDataStructure.push("Rick");
+        stackDataStructure.push("Sandrone");
+        stackDataStructure.push("Lucas");
+        stackDataStructure.push("John");
+        stackDataStructure.push("Faith");
+        stackDataStructure.push("Fely");
+        stackDataStructure.push("Frank");
+        stackDataStructure.push("Samantha");
+        stackDataStructure.push("Ester");
+        stackDataStructure.push("Bernie");
 
         displayNames();
     }
